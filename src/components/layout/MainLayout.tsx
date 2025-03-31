d1bee351cbfa1d1adc1e5a69c887c943d69bdce7
oyerohabib <oyerohabib@yahoo.com>
2025-03-31 22:46:08 +0100

    fix: removed fmr sidebar

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,5 +1,4 @@
 import { ReactNode } from "react";
-import { Sidebar } from "./Sidebar";
 import { Header } from "./Header";
 
 interface MainLayoutProps {
@@ -9,7 +8,6 @@ interface MainLayoutProps {
 export function MainLayout({ children }: MainLayoutProps) {
   return (
     <div className="flex h-screen bg-white">
-      <Sidebar />
       <div className="flex flex-col flex-1 overflow-hidden">
         <Header />
         <main className="flex-1 overflow-auto px-8 py-6">{children}</main>
