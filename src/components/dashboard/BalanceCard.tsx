0fa6121cd49dbf24e9fe0a0f1a2dfaa20e6ed440
oyerohabib <oyerohabib@yahoo.com>
2025-04-01 23:26:22 +0100

    fix: tweaked balanceCard componnt to be more flexible

--- src/components/dashboard/BalanceCard.tsx
+++ src/components/dashboard/BalanceCard.tsx
@@ -1,11 +1,14 @@
 import { Card, CardHeader, CardTitle } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
+import { InfoIcon } from "../reusable/Icons";
 
 interface BalanceCardProps {
   title: string;
   amount: string;
   showWithdraw?: boolean;
+  infoIcon?: boolean;
+  infoTitle?: string;
   className?: string;
 }
 
@@ -13,14 +16,22 @@ export function BalanceCard({
   title,
   amount,
   showWithdraw = false,
+  infoIcon = false,
+  infoTitle = "Learn More",
   className,
 }: BalanceCardProps) {
   return (
-    <Card className={cn("border-none shadow-none", className)}>
-      <CardHeader className="p-0 pb-2 space-y-0 flex flex-row gap-16 items-center">
-        <CardTitle className="text-sm font-normal text-gray-500">
+    <Card className={cn("border-none shadow-none py-4", className)}>
+      <CardHeader
+        className={`p-0 pb-2 space-y-0 flex flex-row gap-16 ${
+          infoIcon ? "justify-between" : "items-center"
+        }`}
+      >
+        <CardTitle className="text-sm font-normal text-gray-500 gap-2 flex flex-col">
           {title}
-          <div className="text-2xl font-bold text-black">{amount}</div>
+          <div className="text-xl md:text-2xl font-bold text-black">
+            {amount}
+          </div>
         </CardTitle>
         {showWithdraw && (
           <Button
@@ -29,6 +40,11 @@ export function BalanceCard({
           >
             Withdraw
           </Button>
+        )}
+        {infoIcon && (
+          <div title={infoTitle}>
+            <InfoIcon className="text-[#888F95] cursor-pointer" />
+          </div>
         )}
       </CardHeader>
     </Card>
