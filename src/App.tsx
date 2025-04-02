1cc248f6aa9ec5fd0b070154695ffaf2b1458312
oyerohabib <oyerohabib@yahoo.com>
2025-04-02 12:12:25 +0100

    feat: configured routing for NavItems pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
-import { Dashboard } from "./pages/Dashboard";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { Analytics } from "./pages/Analytics";
+import { Revenue } from "./pages/Revenue";
+import { CRM } from "./pages/CRM";
 
 function App() {
   return (
-    <>
-      <Dashboard />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/analytics" element={<Analytics />} />
+        <Route path="/revenue" element={<Revenue />} />
+        <Route path="/crm" element={<CRM />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
