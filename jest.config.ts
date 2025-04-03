9ad7975b7f9f1700abd7345fef845ea030b0fe4b
oyerohabib <oyerohabib@yahoo.com>
2025-04-03 19:44:48 +0100

    chore: installed and configured depencies for testing

---
+++ jest.config.ts
@@ -0,0 +1,26 @@
+export default {
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
+  setupFilesAfterEnv: ["<rootDir>/src/setupTests.ts"],
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
+    "@canvasjs/react-charts": "<rootDir>/src/__mocks__/canvasJSMock.js",
+  },
+  transform: {
+    "^.+\\.tsx?$": [
+      "ts-jest",
+      {
+        tsconfig: "tsconfig.json",
+      },
+    ],
+  },
+  transformIgnorePatterns: ["/node_modules/(?!@canvasjs)/"],
+  testMatch: ["**/__tests__/**/*.test.(ts|tsx)"],
+  collectCoverageFrom: [
+    "src/**/*.{ts,tsx}",
+    "!src/**/*.d.ts",
+    "!src/main.tsx",
+    "!src/vite-env.d.ts",
+  ],
+};
