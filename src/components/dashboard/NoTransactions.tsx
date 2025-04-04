40bbe85b4c0952fa6e833776aa473f75baa6e8ee
oyerohabib <oyerohabib@yahoo.com>
2025-04-04 01:40:01 +0100

    fix: improved transaction empty state component to match figma design

--- src/components/dashboard/NoTransactions.tsx
+++ src/components/dashboard/NoTransactions.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@/components/ui/button";
+import { ReceiptLongIcon } from "../reusable/Icons";
 
 interface NoTransactionsProps {
   onClearFilter: () => void;
@@ -6,11 +7,21 @@ interface NoTransactionsProps {
 
 export function NoTransactions({ onClearFilter }: NoTransactionsProps) {
   return (
-    <div className="flex flex-col items-center justify-center py-12 px-4">
-      <p className="text-gray-500 text-center mb-4">
+    <div className="flex flex-col gap-6 items-start py-12 px-4 max-w-[500px] mx-auto">
+      <div className="p-4 rounded-xl bg-[#E1E4EA] w-fit">
+        <ReceiptLongIcon />
+      </div>
+
+      <p className="text-3xl font-bold">
         No matching transactions found for the selected filter
       </p>
-      <Button onClick={onClearFilter} variant="outline">
+      <p>Change your filters to see more results, or add a new product.</p>
+      <Button
+        onClick={onClearFilter}
+        variant="outline"
+        size={"xl"}
+        className="rounded-full bg-[#EFF1F6] cursor-pointer"
+      >
         Clear Filter
       </Button>
     </div>
